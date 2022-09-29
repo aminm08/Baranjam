7f0e2744105da72b8a6af215455d752cf9bd2d92
amin <maf081378@gmail.com>
2022-09-29 20:54:54 +0330

    add translate

--- pages/urls.py
+++ pages/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.homepage, name='homepage')
+    path('', views.homepage, name='homepage'),
+    path('contact_us/', views.contact_us, name='contact_us'),
+    path('about_us/', views.about_us, name='about_us')
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def homepage(request):
     return render(request, 'homepage.html')
+
+
+def about_us(request):
+    return render(request, 'about_us.html')
+
+
+def contact_us(request):
+    return render(request, 'contact_us.html')
