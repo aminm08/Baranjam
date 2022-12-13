9169b2ceb50920580f741e73118392002be37ea3
محمد امین فروزان <maf081378@gmail.com>
2022-12-13 11:30:09 +0330

    add more admin features

--- todo/admin.py
+++ todo/admin.py
@@ -3,14 +3,10 @@ from django.contrib import admin
 from .models import Todo, Job
 
 
-class JobInline(admin.TabularInline):
-    model = Job
-
-
 @admin.register(Todo)
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ['name', 'datetime_created', 'user']
-    inlines = [JobInline, ]
+    list_display = ['name', 'datetime_created', 'user', 'is_group_list']
+    Todo.is_group_list.boolean = True
 
 
 @admin.register(Job)
@@ -18,6 +14,3 @@ class JobAdmin(admin.ModelAdmin):
     list_display = ['text', 'todo', 'user', 'is_done', 'datetime_created']
 
     search_fields = ('text', 'user__username',)
-
-
-
