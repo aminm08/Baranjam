09d0ee8870d4b5df645e343d7bc88cd2db2e4c43
محمد امین فروزان <maf081378@gmail.com>
2023-03-04 20:30:23 +0330

    add middleware to force admin to run in english

--- config/settings.py
+++ config/settings.py
@@ -66,6 +66,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'pages.middleware.AdminLocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
